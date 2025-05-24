fbfbfd42f638626fb42c43a2e417c47ca4e411f7
MaFiSoft <mafi82290@online.de>
2025-05-24 21:54:19 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://repo1.maven.org/maven2/") } // Explizite Maven Central URL
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.5.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
-        classpath("com.google.devtools.ksp:gradle-plugin:2.0.0-1.0.24") // Korrigierte KSP-Abhängigkeit
+        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.0.0-1.0.24") // Korrigierte KSP-Abhängigkeit
     }
 }
 
