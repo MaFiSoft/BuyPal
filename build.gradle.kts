3ad62700f8d167b82a30036adbac7a3fa2455a0e
MaFiSoft <mafi82290@online.de>
2025-05-24 13:56:05 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+        classpath("com.google.devtools.ksp:ksp-gradle-plugin:2.0.0-1.0.24")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
