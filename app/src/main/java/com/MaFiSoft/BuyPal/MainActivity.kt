90ffef160f965f63190a87be943e0e6a2106750f
MaFiSoft <mafi82290@online.de>
2025-05-25 01:42:05 +0200

    Update MainActivity.kt

--- app/src/main/java/com/MaFiSoft/BuyPal/MainActivity.kt
+++ app/src/main/java/com/MaFiSoft/BuyPal/MainActivity.kt
@@ -1,4 +1,4 @@
-// app/src/main/java/com/MaFiSoft/BuyPal/MainActivity.kt - gemini
+// app/src/main/java/com/MaFiSoft/BuyPal/MainActivity.kt
 package com.MaFiSoft.BuyPal
 
 import android.os.Bundle
@@ -11,12 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.MaFiSoft.BuyPal.ui.theme.BuyPalTheme
+// WICHTIG: Korrekter Import für dein Theme
+import com.MaFiSoft.BuyPal.ui.theme.BuyPalTheme // Stelle sicher, dass dieser Pfad korrekt ist!
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // Hier wird das Theme angewendet
             BuyPalTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
