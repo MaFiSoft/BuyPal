494103e166aeafc5d640a6c627575742dd7ad1be
MaFiSoft <mafi82290@online.de>
2025-05-24 22:11:05 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,24 @@
+// build.gradle.kts (Project-Level) - gemini
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
 buildscript {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
     dependencies {
+        // Android Gradle Plugin (AGP) - sehr wichtig für die Kompatibilität
         classpath("com.android.tools.build:gradle:8.5.2")
+        // Kotlin Gradle Plugin - muss zur Kotlin-Version passen
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
-        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.0.0-1.0.24") // Korrigierte KSP-Abhängigkeit
+        // KSP Plugin - muss zur Kotlin-Version passen
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.0.24-2.0.0")
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    // Diese Plugins werden im Modul-build.gradle.kts angewendet
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("com.google.devtools.ksp") version "1.0.24-2.0.0" apply false
 }
