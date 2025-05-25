c72ba3e765958ab8afca69611a9e4348886c9422
MaFiSoft <mafi82290@online.de>
2025-05-25 02:37:24 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.plugin.compose")
-    id("androidx.room") // <--- DIES IST DAS WICHTIGSTE FEHLENDE TEIL
+    id("org.jetbrains.kotlin.plugin.compose") 
 }
 
 android {
