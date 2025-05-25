a8a8230e8c3c972492badd3bd14afcf29d9dfd78
MaFiSoft <mafi82290@online.de>
2025-05-25 22:08:26 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,4 +14,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
     // NEU: Compose Compiler Plugin hier deklarieren
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false // Versionsnummer wie Kotlin-Version
+    // BEGINN DER HINZUFÜGUNG FÜR GOOGLE SERVICES PLUGIN
+    id("com.google.gms.google-services") version "4.4.1" apply false // <-- Diese Zeile hinzufügen/prüfen
+    // ENDE DER HINZUFÜGUNG
 }
